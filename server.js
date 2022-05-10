6e761469d088c931040ae4935de2f0c031125e86
Khizar Shaikh <khizarshaikh2922@gmail.com>
2022-05-10 11:01:21 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+const app = express();
+const http = require("http").createServer(app);
+const port = 4240
+http.listen(port, '0.0.0.0', () =>
+    console.log(`listening on http://localhost:${port}`));
+
+app.use("/Asset", express.static(__dirname + "/Asset"));
+app.get("/home",(req,res)=>{
+    res.sendFile(__dirname+`/tabs/index.html`)
+})
+app.get("/home/studentlogin",(req,res)=>{
+    res.sendFile(__dirname+`/tabs/studentlogin.html`)
+})
+app.get("/home/teacherlogin",(req,res)=>{
+    res.sendFile(__dirname+`/tabs/teacherlogin.html`)
+})
+app.get("/admin",(req,res)=>{
+    res.sendFile(__dirname+`/tabs/admin.html`)
+}) 
