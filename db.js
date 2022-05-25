e11b7f1451ef31416aadd4399f71a195fb441f4e
Khizar Shaikh <khizarshaikh2922@gmail.com>
2022-05-25 18:49:09 +0530

    added insert api , login , view api

--- db.js
+++ db.js
@@ -1,33 +1,65 @@
+const res = require("express/lib/response");
 const { MongoClient } = require("mongodb");
-
+const Realm = require('realm-web');
 const uri = "mongodb+srv://khizar:Khizar2922@cluster0.ijkkm.mongodb.net/?retryWrites=true&w=majority";
 const databaseName = "iNueron";
+const app = new Realm.App({ id: "inueron-leylz" });
 
 MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
   if (error) {
     return console.log("Connection failed for some reason");
   }
+  
   console.log("Connection established - All well");
   const db = client.db(databaseName);
-  exports.user_ins = (insobj) => {
-    db.collection('admin').insertOne(insobj, function (err, result) {
-      if (err) {
-        throw err
-      }
+  
 
-      console.log("inserted")
-    })
-    console.log("value", insobj['_id'])
-  }
-  exports.studentque = (insobj) => {
-    db.collection('admin').insertOne(insobj, function (err, result) {
-      if (err) {
-        return "err"
+  exports.loginEmailPassword = async (email, password) => {
+    return new Promise(async (resolve, reject)=>{
+      const credentials = Realm.Credentials.emailPassword(email, password);
+      try {
+        // Authenticate the user
+        const user = await app.logIn(credentials);
+        console.log("this is",email,password)
+        // `App.currentUser` updates to match the logged in user
+        console.assert(user.id === app.currentUser.id,'here');
+        resolve(user);
       } 
-        console.log("done1")
-        return insobj.success = "done"
-      
+       catch (err) {
+        console.log("Failed to log in"+err);
+        resolve({Error:err['error'],errCode:err['errorCode'],statusCode:err['statusCode']});
+      }
+    })
+    
+    // catch (err) {
+    //   console.error("Failed to log in", err);
+    // }
+  }
+
+  exports.getAllAproveStudent = () =>{
+    return new Promise((resolve,reject)=>{
+    db.collection('studentApprove').find().toArray((err,result)=>{
+        if(err){
+          reject('can not find it '+err)
+        }
+        // console.log(result)
+        resolve(result)
+      })
     })
   }
+
+  exports.studentque = (insobj) => {
+    return new Promise((resolve, reject) => {
+      db.collection('admin').insertOne(insobj, function (err, result) {
+        if (err) {
+          throw err
+        }
+        console.log("at db.js")
+        resolve(result)
+      })
+
+    })
+  }
+
 });
 //   })
